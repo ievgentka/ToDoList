235c8f2573ecf5080707a8033dbecc68ef8f8e81
Ievgen Tkachenko <ievgtka@gmail.com>
2022-08-18 23:23:19 +0200

    Add testCreateAndDeleteTasks, testRenameTask

---
+++ ToDoListUITests/Tests/iOS/TasksTests.swift
@@ -0,0 +1,73 @@
+//
+//  TasksTests.swift
+//  ToDoListUITests
+//
+//  Created by Eugene Tkachenko on 18.08.2022.
+//  Copyright © 2022 Radu Ursache - RanduSoft. All rights reserved.
+//
+
+import XCTest
+
+class TasksTests: BaseTest {
+    
+    func testCreateAndDeleteTasks() {
+        let homeScreen = launchApp
+        
+        let englishString = CommonHelpers.randomString(charset: .english, length: 30)
+        let сyrillicString = CommonHelpers.randomString(charset: .сyrillic, length: 30)
+        
+        homeScreen
+            .addTask
+            .addTaskContent(englishString)
+            .saveTask()
+        XCTAssertEqual(homeScreen.tasksQuantity(.all), "1", "Incorrect number of existing tasks")
+
+        homeScreen
+            .addTask
+            .addTaskContent(сyrillicString)
+            .saveTask()
+        XCTAssertEqual(homeScreen.tasksQuantity(.all), "2", "Incorrect number of existing tasks")
+        
+        let allTasksScreen = homeScreen.openTaskList(screen: .all)
+        XCTAssertEqual(allTasksScreen.countTasks, 2)
+        
+        allTasksScreen
+            .getTask(name: englishString)
+            .delete
+            .getTask(name: сyrillicString)
+            .delete
+        // Reopening the screen due to an existing bug in the app: The UI is updating, but tree of elements updates only after screen reopen
+        _ = allTasksScreen.openHomeScreen.openTaskList(screen: .all)
+        XCTAssertEqual(allTasksScreen.countTasks, 0, "Incorrect number of existing tasks")
+    }
+    
+    func testRenameTask() {
+        let homeScreen = launchApp
+        
+        let content = CommonHelpers.randomString(charset: .english, length: 30)
+        let newContent = CommonHelpers.randomString(charset: .english, length: 30)
+        
+        homeScreen
+            .addTask
+            .addTaskContent(content)
+            .saveTask()
+        XCTAssertEqual(homeScreen.tasksQuantity(.all), "1", "Incorrect number of existing tasks")
+        
+        let allTasksScreen = homeScreen.openTaskList(screen: .all)
+        allTasksScreen
+            .getTask(name: content)
+            .select
+            .openEditOption
+            .clearAndAddTaskContent(newContent)
+            .saveTask()
+        
+        let taskWithNewContent = allTasksScreen.getTask(name: newContent)
+        XCTAssertTrue(taskWithNewContent.exists, "Task with new content: \(newContent) does not exist")
+        XCTAssertEqual(allTasksScreen.countTasks, 1, "Incorrect number of existing tasks")
+        
+        taskWithNewContent.delete
+        // Reopening the screen due to an existing bug in the app: The UI is updating, but tree of elements updates only after screen reopen
+        _ = allTasksScreen.openHomeScreen.openTaskList(screen: .all)
+        XCTAssertEqual(allTasksScreen.countTasks, 0, "Incorrect number of existing tasks")
+    }
+}
